6323c3686edc091241e85506e8d348bdc504d6b9
sankethn@creditnirvana.ai <sankethn@creditnirvana.ai>
2026-05-13 10:58:26 +0530

    day 3

---
+++ DSA-Noolee/src/lc/LeetCode49.java
@@ -0,0 +1,26 @@
+package lc;
+
+import java.util.*;
+
+public class LeetCode49 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        //#Brute force
+        List<List<String>> res = new ArrayList<>();
+        Map<String,List<String>> uni = new HashMap<>();
+        for(int i =0; i<strs.length;i++){
+            char[] str =strs[i].toCharArray();
+            Arrays.sort(str);
+            String sortedStr = String.valueOf(str);
+            if(!uni.containsKey(sortedStr)){
+                uni.put(sortedStr,new ArrayList<String>());
+            }
+            uni.get(sortedStr).add(strs[i]);
+        }
+        for(String s : uni.keySet()){
+            res.add(uni.get(s));
+        }
+
+        return res;
+    }
+
+}
