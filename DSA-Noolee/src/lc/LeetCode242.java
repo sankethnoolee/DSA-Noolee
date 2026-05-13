768f7eb2d6e4eb1ac6dd893958b00ef1e44fe76e
sankethn@creditnirvana.ai <sankethn@creditnirvana.ai>
2026-05-13 09:54:57 +0530

    day 2

--- DSA-Noolee/src/lc/LeetCode242.java
+++ DSA-Noolee/src/lc/LeetCode242.java
@@ -6,8 +6,36 @@ import java.util.HashSet;
 import java.util.Map;
 
 public class LeetCode242 {
+    public static void main(String[] args) {
+        LeetCode242 lc= new LeetCode242();
+        lc.isAnagram("cat","rat");
+    }
     public boolean isAnagram(String s, String t) {
+        // METHOD 4
+        //In the prog its given only lower case letters so we map counts of char in arr as below
+        int[] az = new int[26];
+        //az hold each char by index a-0 , z-25
+        int sl = s.length();
+        int tl = t.length();
+        if(sl!=tl){
+            return false;
+        }
+        for(int i = 0; i<sl; i++){
+            //inc first string
+            az[s.charAt(i)-'a']++;
+        }
+        for(int i = 0; i<tl; i++){
+            //dec second string
+            az[t.charAt(i)-'a']--;
+            //the final arr shouldn't go below zero which indicates some char is more in second str
+            if(az[t.charAt(i)-'a']<0){
+                return false;
+            }
+        }
+        return true;
 
+        /*
+        METHOD 3
         char[] sArr = s.toCharArray();
         char[] tArr = t.toCharArray();
 
@@ -18,7 +46,10 @@ public class LeetCode242 {
         Arrays.sort(tArr);
 
         return Arrays.equals(sArr,tArr);
+
+         */
         /*
+        METHOD 2
         can avoid for loop if inbuild func is used
 
         for(int i = 0; i< sArr.length; i++) {
@@ -30,6 +61,7 @@ public class LeetCode242 {
         */
 
         /*
+        METHOD 1
         using hashmaps
 
         if(s.length()!=t.length()){
