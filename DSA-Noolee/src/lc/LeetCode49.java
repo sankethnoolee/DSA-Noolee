090ac42629d9ad1481c1fd425ff6d9e92b8d9403
sankethnoolee@gmail.com <sankethnoolee@gmail.com>
2026-05-14 11:17:20 +0530

    day 3 optimised

--- DSA-Noolee/src/lc/LeetCode49.java
+++ DSA-Noolee/src/lc/LeetCode49.java
@@ -4,8 +4,22 @@ import java.util.*;
 
 public class LeetCode49 {
     public List<List<String>> groupAnagrams(String[] strs) {
-        //#TODO  HASHING TECHNIQUE
+        //Trying by hashing string instead of sorting each string
+        //although my earlier solution gave faster in leetcode
+        //this frquency based approach.
 
+        Map<String,List<String>> uni = new HashMap<>();
+        for(String s : strs){
+            String hash = hashMyString(s);
+            uni.computeIfAbsent(hash, k -> new ArrayList<>()).add(s);
+            if(!uni.containsKey(hash)){
+                uni.put(hash,new ArrayList<>());
+            }
+            uni.get(hash).add(s);
+        }
+
+        return new ArrayList<>(uni.values());
+        /*
         //#Brute force
         List<List<String>> res = new ArrayList<>();
         Map<String,List<String>> uni = new HashMap<>();
@@ -23,6 +37,21 @@ public class LeetCode49 {
         }
 
         return res;
+        */
+    }
+
+    private String hashMyString(String s) {
+
+        //just auto boxing improved the perf by 25%
+        int [] ia = new int[26];
+        for(Character c : s.toCharArray()){
+            ia[c-'a']++;
+        }
+        StringBuilder sb = new StringBuilder();
+        for(Integer i : ia){
+            sb.append(i).append("#");
+        }
+        return sb.toString();
     }
 
 }
