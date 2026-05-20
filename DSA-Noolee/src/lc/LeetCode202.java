664323bf3aa5d3dd2db1de4e391b1a988a9f1957
sankethnoolee@gmail.com <sankethnoolee@gmail.com>
2026-05-20 10:55:35 +0530

    day 9

--- DSA-Noolee/src/lc/LeetCode202.java
+++ DSA-Noolee/src/lc/LeetCode202.java
@@ -1,6 +1,21 @@
 package lc;
 
 public class LeetCode202 {
+
+    public boolean isHappy(int n) {
+        int x = 0;
+        while (n > 0) {
+            x += ((n % 10) * (n % 10));
+            n = n / 10;
+        }
+        if (x == 1)
+            return true;
+        if (x == 4)
+            return false;
+
+        return isHappy(x);
+    }
+    /*
     //This is BRUTE FORCE
     //TODO revist after theory
     public static void main(String[] args) {
@@ -40,4 +55,6 @@ public class LeetCode202 {
             return sum;
 
     }
+
+     */
 }
