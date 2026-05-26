4ad981303b05382ca0db917633d7c33b2f0965dd
sankethnoolee@gmail.com <sankethnoolee@gmail.com>
2026-05-26 10:50:36 +0530

    day 15

---
+++ DSA-Noolee/src/lc/LeetCode263.java
@@ -0,0 +1,11 @@
+package lc;
+
+public class LeetCode263 {
+    public boolean isUgly(int num) {
+        if(num < 1) return false;
+        while(num%2 ==0) num = num/2;
+        while(num%3 == 0) num = num/3;
+        while(num%5 == 0) num = num/5;
+        return num == 1;
+    }
+}
