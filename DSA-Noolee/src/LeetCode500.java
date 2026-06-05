3d146aebde0da2581f37168ed0bb74b19fcbdb40
sankethnoolee@gmail.com <sankethnoolee@gmail.com>
2026-06-05 10:47:22 +0530

    day 25

---
+++ DSA-Noolee/src/LeetCode500.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetCode500 {
+    public String[] findWords(String[] words) {
+        List<String> res = new ArrayList<String>();
+        String r1 = "QWERTYUIOPqwertyuiop";
+        String r2 = "ASDFGHJKLasdfghjkl";
+        String r3 = "ZXCVBNMzxcvbnm";
+        int i =0 ;
+        for(String s : words){
+            if(s.matches("[" + r1 + "]+") || s.matches("[" + r3 + "]+") || s.matches("[" + r2 + "]+")){
+                res.add(s);
+                i++;
+            }
+        }
+        return res.toArray(String[]::new);
+    }
+}
