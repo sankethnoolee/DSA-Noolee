5e0bf1b2c058ba7efa2a87209878a85276477b89
sankethnoolee@gmail.com <sankethnoolee@gmail.com>
2026-06-08 17:42:17 +0530

    day 26

---
+++ DSA-Noolee/src/lc/LeetCode520.java
@@ -0,0 +1,14 @@
+package lc;
+
+public class LeetCode520 {
+    public boolean detectCapitalUse(String word) {
+        int cnt = 0;
+        for (char c : word.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                ++cnt;
+            }
+        }
+        return cnt == 0 || cnt == word.length()
+                || (cnt == 1 && Character.isUpperCase(word.charAt(0)));
+    }
+}
