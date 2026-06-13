5d9bd52f2c662caefa469edc7f4da8e29cfdb074
sankethnoolee@gmail.com <sankethnoolee@gmail.com>
2026-06-13 21:54:08 +0530

    day 31

---
+++ DSA-Noolee/src/lc/LeetCode14.java
@@ -0,0 +1,15 @@
+package lc;
+
+public class LeetCode14 {
+    public String longestCommonPrefix(String[] strs) {
+        int n = strs.length;
+        for (int i = 0; i < strs[0].length(); ++i) {
+            for (int j = 1; j < n; ++j) {
+                if (strs[j].length() <= i || strs[j].charAt(i) != strs[0].charAt(i)) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+}
