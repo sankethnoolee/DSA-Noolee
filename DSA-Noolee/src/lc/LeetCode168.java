4b2e59e061f4fa58e52bd77fec5888c7c36c1dad
sankethnoolee@gmail.com <sankethnoolee@gmail.com>
2026-06-19 19:06:21 +0530

    day 36

---
+++ DSA-Noolee/src/lc/LeetCode168.java
@@ -0,0 +1,16 @@
+package lc;
+
+public class LeetCode168 {
+    public String convertToTitle(int columnNumber) {
+        StringBuilder result = new StringBuilder();
+
+        while (columnNumber > 0) {
+            columnNumber--;
+            int remainder = columnNumber % 26;
+            result.append((char) ('A' + remainder));
+            columnNumber /= 26;
+        }
+
+        return result.reverse().toString();
+    }
+}
