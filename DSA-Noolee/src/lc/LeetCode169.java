3ed8e48c00c6bc19afe4bf34e740f2fb9a21a830
sankethnoolee@gmail.com <sankethnoolee@gmail.com>
2026-06-20 20:07:25 +0530

    day 37

---
+++ DSA-Noolee/src/lc/LeetCode169.java
@@ -0,0 +1,21 @@
+package lc;
+
+public class LeetCode169 {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return candidate;
+    }
+}
