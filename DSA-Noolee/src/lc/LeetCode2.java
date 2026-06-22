55ddec664064fea0bba99ff48ecfb87a06a48dc5
sankethnoolee@gmail.com <sankethnoolee@gmail.com>
2026-06-22 13:59:26 +0530

    day 38

---
+++ DSA-Noolee/src/lc/LeetCode2.java
@@ -0,0 +1,40 @@
+package lc;
+
+public class LeetCode2 {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode temp = new ListNode(0);
+        ListNode curr = temp;
+
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+
+            int sum = carry;
+
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            carry = sum / 10;
+
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+        }
+
+        return temp.next;
+    }
+}
+ class ListNode {
+     int val;
+     ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
