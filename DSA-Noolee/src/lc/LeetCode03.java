236560ea550f3c00819a8ad55c774f658006977e
sankethnoolee@gmail.com <sankethnoolee@gmail.com>
2026-06-23 10:41:56 +0530

    day 38

--- DSA-Noolee/src/lc/LeetCode03.java
+++ DSA-Noolee/src/lc/LeetCode03.java
@@ -12,22 +12,22 @@ public class LeetCode03 {
     }
 
     public int lengthOfLongestSubstring(String s) {
-        char[] cArr = s.toCharArray();
-        int max = 0;
-        int sub = 0;
         Set<Character> set = new HashSet<>();
-        for(int i = 0;i<cArr.length;i++){
-            if(!set.add(cArr[i])){
-                set=new HashSet<>();
-                set.add(cArr[i]);
-                max = Math.max(max,sub);
-                sub=1;
 
-            }else{
-                sub++;
-                max = Math.max(max,sub);
+        int left = 0;
+        int max = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            while (set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
             }
+
+            set.add(s.charAt(right));
+            max = Math.max(max, right - left + 1);
         }
+
         return max;
     }
 }
