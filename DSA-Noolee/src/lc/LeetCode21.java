9b1e90b734715e508c9a5cd9b80967adc4aff52a
sankethnoolee@gmail.com <sankethnoolee@gmail.com>
2026-06-25 12:21:52 +0530

    day 40

--- DSA-Noolee/src/lc/LeetCode21.java
+++ DSA-Noolee/src/lc/LeetCode21.java
@@ -6,6 +6,37 @@ import java.util.List;
 
 public class LeetCode21 {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        //using array merge logic
+        ListNode temp = new ListNode(0);
+        ListNode curr = temp;
+
+        while (l1 != null && l2 != null) {
+
+            if (l1.val <= l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+
+            curr = curr.next;
+        }
+
+        if (l1 != null) {
+            curr.next = l1;
+        }
+
+        if (l2 != null) {
+            curr.next = l2;
+        }
+
+        return temp.next;
+    }
+
+
+        /*
+        //BRUTE FORCE
         List<Integer> res = new ArrayList<>();
         if(l1==null && l2==null){
             return null;
@@ -31,5 +62,5 @@ public class LeetCode21 {
         }
         return temp.next;
 
-    }
+    }*/
 }
