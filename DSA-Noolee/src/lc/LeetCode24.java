6e6b8440c6cede711afec27a858d6412063d7782
sankethnoolee@gmail.com <sankethnoolee@gmail.com>
2026-06-26 12:05:17 +0530

    day 41

---
+++ DSA-Noolee/src/lc/LeetCode24.java
@@ -0,0 +1,23 @@
+package lc;
+
+public class LeetCode24 {
+    public ListNode swapPairs(ListNode head) {
+        ListNode temp = new ListNode(0);
+        temp.next = head;
+        ListNode prev = temp;
+
+        while(prev.next!=null && prev.next.next!=null){
+
+            ListNode first = prev.next;
+            ListNode second = first.next;
+
+            first.next = second.next;
+            second.next = first;
+            prev.next = second;
+            prev = first;
+
+
+        }
+        return temp.next;
+    }
+}
