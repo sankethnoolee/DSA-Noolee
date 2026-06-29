3a4fadd249d4b23b8b54501e74031a339c4b58fb
sankethnoolee@gmail.com <sankethnoolee@gmail.com>
2026-06-29 09:54:04 +0530

    day 43

---
+++ DSA-Noolee/src/lc/LeetCode203.java
@@ -0,0 +1,20 @@
+package lc;
+
+public class LeetCode203 {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode temp = new ListNode(0);
+        temp.next = head;
+
+        ListNode current = temp;
+
+        while (current.next != null) {
+            if (current.next.val == val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+
+        return temp.next;
+    }
+}
