13691dbd8d14eafed5642b17a81c62b575d27f25
sankethnoolee@gmail.com <sankethnoolee@gmail.com>
2026-06-30 14:07:26 +0530

    day 43

---
+++ DSA-Noolee/src/lc/LeetCode206.java
@@ -0,0 +1,16 @@
+package lc;
+
+public class LeetCode206 {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+}
