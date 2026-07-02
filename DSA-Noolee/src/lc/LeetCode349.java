948e1bf4f9c72c5b1441b55bede615a1e7f35655
sankethnoolee@gmail.com <sankethnoolee@gmail.com>
2026-07-02 10:47:32 +0530

    day 45

---
+++ DSA-Noolee/src/lc/LeetCode349.java
@@ -0,0 +1,28 @@
+package lc;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LeetCode349 {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        for (int num : nums1) {
+            set1.add(num);
+        }
+
+        Set<Integer> intersectionSet = new HashSet<>();
+        for (int num : nums2) {
+            if (set1.contains(num)) {
+                intersectionSet.add(num);
+            }
+        }
+
+        int[] result = new int[intersectionSet.size()];
+        int i = 0;
+        for (int num : intersectionSet) {
+            result[i++] = num;
+        }
+
+        return result;
+    }
+}
