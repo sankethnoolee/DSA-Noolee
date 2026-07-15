69ed8fa6ed7e8182924015ba439047cbd6969c51
sankethnoolee@gmail.com <sankethnoolee@gmail.com>
2026-07-15 09:12:14 +0530

    day 50

---
+++ DSA-Noolee/src/lc/LeetCode3658.java
@@ -0,0 +1,9 @@
+package lc;
+
+public class LeetCode3658 {
+    public int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+    //because sum of odd is n2 and sum of even is n(n+1)
+    //and gcd of these is n itself
+}
