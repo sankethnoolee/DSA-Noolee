11fc5d00f60e0f47b2fc3c9eecaccabe0344ca20
sankethnoolee@gmail.com <sankethnoolee@gmail.com>
2026-07-18 16:55:06 +0530

    day 52

---
+++ DSA-Noolee/src/lc/LeetCode492.java
@@ -0,0 +1,11 @@
+package lc;
+
+public class LeetCode492 {
+    public int[] constructRectangle(int area) {
+        int w = (int)Math.sqrt(area);
+        while(area%w!=0){
+            w--;
+        }
+        return new int[]{area/w,w};
+    }
+}
