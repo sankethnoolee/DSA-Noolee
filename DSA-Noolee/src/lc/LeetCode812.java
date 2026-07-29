31fff8538e2229da997cbf99f8a6c0728e1c96b6
sankethnoolee@gmail.com <sankethnoolee@gmail.com>
2026-07-29 14:02:08 +0530

    day 64

---
+++ DSA-Noolee/src/lc/LeetCode812.java
@@ -0,0 +1,25 @@
+package lc;
+
+public class LeetCode812 {
+    public double largestTriangleArea(int[][] points) {
+        double res = 0;
+
+        int n = points.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+
+                    double area = Math.abs(
+                            points[i][0] * (points[j][1] - points[k][1]) +
+                                    points[j][0] * (points[k][1] - points[i][1]) +
+                                    points[k][0] * (points[i][1] - points[j][1])
+                    ) / 2.0;
+
+                    res = Math.max(res, area);
+                }
+            }
+        }
+        return res;
+    }
+}
