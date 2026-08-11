9d2580523841545d6b503fb9dd6a31dcf1c68331
sankethnoolee@gmail.com <sankethnoolee@gmail.com>
2026-08-11 09:46:23 +0530

    day 74

---
+++ DSA-Noolee/src/lc/LeetCode821.java
@@ -0,0 +1,26 @@
+package lc;
+
+public class LeetCode821 {
+    public int[] shortestToChar(String s, char c) {
+        int n = s.length();
+        int[] ans = new int[n];
+
+        int prev = -n;
+
+        for(int i = 0;i<n;i++){
+            if(s.charAt(i)==c){
+                prev = i;
+            }
+            ans[i] = i-prev;
+        }
+
+        for(int i = prev-1; i>=0;i--){
+            if(s.charAt(i)==c){
+                prev = i;
+            }
+            ans[i] = Math.min(ans[i],prev-i);
+        }
+
+        return ans;
+    }
+}
