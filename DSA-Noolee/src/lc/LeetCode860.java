6533bc583d843050fa7844783b4d2092df09e055
sankethnoolee@gmail.com <sankethnoolee@gmail.com>
2026-08-14 16:30:47 +0530

    day 74

---
+++ DSA-Noolee/src/lc/LeetCode860.java
@@ -0,0 +1,31 @@
+package lc;
+
+public class LeetCode860 {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0;
+        int ten = 0;
+
+        for (int bill : bills) {
+            if (bill == 5) {
+                five++;
+            } else if (bill == 10) {
+                if (five == 0) {
+                    return false;
+                }
+                five--;
+                ten++;
+            } else { // bill == 20
+                // Greedy choice: Always try to give one $10 and one $5 first
+                if (ten > 0 && five > 0) {
+                    ten--;
+                    five--;
+                } else if (five >= 3) {
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
